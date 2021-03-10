a449a5f3345ca1c975f811e6eb4ba849d3e1be25
cristi <tataru.cristi5@gmail.com>
2021-03-10 18:44:35 +0200

    first commit

---
+++ employeemanager/src/main/java/com/tataru/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tataru.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
